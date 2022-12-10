910164944a7fccc984bd91c2157351f165cc9bb2
Kevin Simpson <simpkevin@gmail.com>
2022-12-10 12:35:10 -0800

    paritially working controls

---
+++ controls.js
@@ -0,0 +1,107 @@
+let controlState = {
+    running: true
+}
+
+function init_controls(){
+    if (agent.timer === null) {
+        agent.timer = setInterval(q_learning_policy, agent.timer_interval)
+    }
+    let currentSpeed = localStorage.getItem('agent_speed')
+    agent.timer_interval = agent.timer_interval_options[currentSpeed]
+    let visualize_q_val = localStorage.getItem('visualize_q')
+    if(visualize_q_val === 'true'){
+        agent.visualize_q=true
+    }
+    if(agent.visualize_q){
+        $('#control_visualization').prop('checked', true)
+        visualize_q()
+    }
+    $('#control_epoch').val('Epoch = ' + get_epoch())
+}
+
+function render_controls() {
+    if (controlState.running) {
+        reset_timer()
+        $('#control_start').prop('disabled', true)
+        $('#control_stop').prop('disabled', false)
+        if (get_current_speed_index() === 0) {
+            $('#control_slower').prop('disabled', false)
+            $('#control_faster').prop('disabled', true)
+        } else if (get_current_speed_index() + 1 < agent.timer_interval_options.length) {
+            $('#control_slower').prop('disabled', false)
+            $('#control_faster').prop('disabled', false)
+        } else {
+            $('#control_slower').prop('disabled', true)
+            $('#control_faster').prop('disabled', false)
+        }
+        localStorage.setItem('agent_speed', get_current_speed_index())
+        localStorage.setItem('visualize_q', agent.visualize_q)
+    } else {
+        $('#control_start').prop('disabled', false)
+        $('#control_stop').prop('disabled', true)
+    }
+}
+
+function reset_timer() {
+    if (agent.timer !== null) {
+        clearInterval(agent.timer)
+    }
+    if (controlState.running) {
+        agent.timer = setInterval(q_learning_policy, agent.timer_interval)
+    }
+}
+
+function get_current_speed_index() {
+    let currentSpeedIndex = agent.timer_interval_options.findIndex((item) => {
+        return item >= agent.timer_interval
+    })
+    return currentSpeedIndex
+}
+
+$('#control_stop').click(function () {
+    controlState.running = false
+    clearInterval(agent.timer)
+    agent.timer = null
+    render_controls()
+})
+
+$('#control_start').click(function () {
+    controlState.running = true
+    render_controls()
+})
+
+$('#control_slower').click(function () {
+    let currentSpeedIndex = agent.timer_interval_options.findIndex((item) => {
+        return item >= agent.timer_interval
+    })
+    // at the end of the list
+    if (currentSpeedIndex === agent.timer_interval_options.length) {
+        return
+    }
+    agent.timer_interval = agent.timer_interval_options[currentSpeedIndex + 1]
+    render_controls()
+})
+
+$('#control_faster').click(function () {
+    let currentSpeedIndex = agent.timer_interval_options.findIndex((item) => {
+        return item >= agent.timer_interval
+    })
+    // at the end of the list
+    if (currentSpeedIndex === 0) {
+        return
+    }
+    agent.timer_interval = agent.timer_interval_options[currentSpeedIndex - 1]
+    render_controls()
+})
+
+$('#control_visualization').click(function(){
+    if($('#control_visualization').prop('checked')){
+        agent.visualize_q = true
+    } else {
+        agent.visualize_q = false
+    }
+    render_controls()
+    setTimeout(() => {
+        location.reload()
+    }, 200)
+})
--- policy.js
+++ policy.js
@@ -46,33 +46,34 @@ function choose_best_action(state) {
     return {'action': qmax_action, 'q': qmax}
 }
 
-function save_policy_state() {
-    // save Q table
-    console.log('SAVING Q table...')
-    let Q_json = JSON.stringify(Q)
-    // let el = document.getElementById('q_learning')
-    // el.value = Q_json
-    let start_time = window.localStorage.getItem('q_learning_start')
-    if (start_time === null) {
-        start_time = Date.now()
-        window.localStorage.setItem('q_learning_start', start_time)
-    }
-    console.log('ELAPSED TIME = ' + (Date.now() - start_time) / 1000 / 3600)
+function get_epoch(){
     let epoch = window.localStorage.getItem('q_learning_epoch')
     if (epoch == null) {
         epoch = 1
     }
     epoch = parseInt(epoch) + 1
-    console.log('EPOCH = ' + epoch)
-    window.localStorage.setItem('q_learning', Q_json)
-    window.localStorage.setItem('q_learning_epoch', epoch)
-    if (epoch < 5) {
-        window.localStorage.setItem('q_learning_' + epoch, Q_json)
-    }
-    if (epoch < 1000 && epoch % 100 === 0) {
-        window.localStorage.setItem('q_learning_' + epoch, Q_json)
-    }
-    if (epoch > 1000 && epoch % 1000 === 0) {
-        window.localStorage.setItem('q_learning_' + epoch, Q_json)
+    return epoch
+}
+
+function save_policy_state() {
+    let Q_json = JSON.stringify(Q)
+    let epoch = get_epoch()
+    epoch = parseInt(epoch) + 1
+    $('#control_epoch').val('Epoch = ' + epoch)
+    if (agent.save_q) {
+        // save Q table
+        console.log('SAVING Q table...')
+        window.localStorage.setItem('q_learning', Q_json)
+        window.localStorage.setItem('q_learning_epoch', epoch)
+
+        if (epoch < 5) {
+            window.localStorage.setItem('q_learning_' + epoch, Q_json)
+        }
+        if (epoch < 1000 && epoch % 100 === 0) {
+            window.localStorage.setItem('q_learning_' + epoch, Q_json)
+        }
+        if (epoch > 1000 && epoch % 1000 === 0) {
+            window.localStorage.setItem('q_learning_' + epoch, Q_json)
+        }
     }
 }
